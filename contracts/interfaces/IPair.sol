fb6a9793bdc020c69f5908562b25bd3f7dc1a7e0
Armando Medina <113925592+armsves@users.noreply.github.com>
2023-07-09 08:21:16 +0200

    Add files via upload

--- contracts/interfaces/IPair.sol
+++ contracts/interfaces/IPair.sol
@@ -1,1 +1,28 @@
+// SPDX-License-Identifier: UNLICENSED
 
+pragma solidity >=0.6.0 <0.9.0;
+
+
+interface IPair {
+    function getReserves() external view returns (uint reserve0, uint reserve1);
+    function token0() external view returns (address);
+    function token1() external view returns (address);
+    event Mint(address indexed sender, uint amount0, uint amount1);
+    event Burn(address indexed sender, uint amount0, uint amount1, address indexed to);
+    event Swap(
+        address indexed sender,
+        uint amount0In,
+        uint amount1In,
+        uint amount0Out,
+        uint amount1Out,
+        address indexed to
+    );
+    event Sync(uint reserve0, uint reserve1);
+
+    function mint(address to) external returns (uint liquidity);
+    function burn(address to) external returns (uint amount0, uint amount1);
+    function swap(uint amount0Out, uint amount1Out, address to) external;
+    function skim(address to) external;
+    function sync() external;
+    function initialize(address, address) external;
+}
